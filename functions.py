27a963d0b96f19a1e8f38c3c1c5eaf19513f3efb
Chase Perlen <cperlen@gmail.com>
2016-10-27 09:40:15 -0400

    Fixed functions to pass initial tests

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import numpy as N
 
+
 def ApproximateJacobian(f, x, dx=1e-6):
     """Return an approximation of the Jacobian Df(x) as a numpy matrix"""
     try:
@@ -8,10 +9,11 @@ def ApproximateJacobian(f, x, dx=1e-6):
         n = 1
     fx = f(x)
     Df_x = N.matrix(N.zeros((n,n)))
+
     for i in range(n):
         v = N.matrix(N.zeros((n,1)))
-        v[i,0] = dx
-        Df_x[:,i] = f(x + v) - fx
+        v[i,0] = dx # derivative of jth component
+        Df_x[:,i] = (f(x + v) - fx)/dx #fixed definition of derivative
     return Df_x
 
 class Polynomial(object):
@@ -30,11 +32,16 @@ class Polynomial(object):
         return "Polynomial(%s)" % (", ".join([str(x) for x in self._coeffs]))
 
     def f(self,x):
-        ans = self._coeffs[0]
-        for c in self._coeffs[1:]:
-            ans = x*ans + c
+        try:
+            deg = len(self._coeffs)
+        except TypeError:
+            deg = 1
+            
+        ans = self._coeffs[deg-1] 
+        for i in range(1,deg):
+            ans = ans + self._coeffs[deg-1-i]*pow(x,i) #fixed polynomial def
         return ans
-
+    
     def __call__(self, x):
         return self.f(x)
 
